11015402783079d0eb42aff51be0192ea51fb83a
gustavofukunaga <gustavofukunaga@usp.br>
2025-05-02 02:32:24 -0300

    tutorial s1e3

---
+++ Scenes/Unit/unit.gd
@@ -0,0 +1,20 @@
+@tool
+class_name Unit
+extends Area2D
+
+@export var stats: UnitStats : set = set_stats
+
+@onready var skin: Sprite2D = $Skin
+@onready var health_bar: ProgressBar = $HealthBar
+
+
+func set_stats(value: UnitStats) -> void:
+	stats = value
+	
+	if value == null:
+		return
+	
+	if not is_node_ready():
+		await ready
+	
+	skin.region_rect.position = Vector2(stats.skin_coordinates) * Arena.CELL_SIZE
